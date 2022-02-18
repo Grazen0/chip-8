3c34f7298ea5606ff396c34b13c1fc8a97f9326d
ElCholoGamer <josedanielgrayson@gmail.com>
2022-02-18 10:27:39 -0500

    refactor: clear project and use static adapter

---
+++ src/lib/components/Title.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let title = 'CHIP-8 Emulator';
+	export let topic: string | undefined = undefined;
+
+	const fullTitle = topic ? `${topic} · ${title}` : title;
+</script>
+
+<svelte:head>
+	<title>{fullTitle}</title>
+</svelte:head>
