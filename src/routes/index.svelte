e79e37c66b5b9cc170eded182df61cbda807b70e
ElCholoGamer <josedanielgrayson@gmail.com>
2022-02-19 11:38:50 -0500

    feat: add page information

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import ControlsDiagram from '$lib/components/ControlsDiagram.svelte';
 	import Emulator from '$lib/components/Emulator.svelte';
 
 	const roms = [
@@ -13,14 +14,48 @@
 		{ file: 'opcode_test.ch8', label: 'Opcode test' },
 		{ file: 'delay_timer_test.ch8', label: 'Delay timer test' },
 		{ file: 'random_number_test.ch8', label: 'Random number test' },
+		{ file: 'misc_test.ch8', label: 'Misc. test' },
+	];
+
+	const links = [
+		{ href: '/', label: 'Home' },
+		{ href: 'https://en.wikipedia.org/wiki/CHIP-8', label: 'CHIP-8 at Wikipedia' },
+		{ href: 'https://github.com/ElCholoGamer/chip-8', label: 'Source' },
 	];
 </script>
 
-<main class="py-14 px-6">
-	<h1 class="text-center text-3xl font-bold">CHIP-8 Emulator</h1>
-	<p class="text-center">By ElCholoGamer</p>
+<main class="py-14 px-6 text-center">
+	<h1 class="text-3xl font-bold">CHIP-8 Emulator</h1>
+	<p>By ElCholoGamer</p>
 
-	<div class="my-12 flex flex-col items-center">
-		<Emulator {roms} />
-	</div>
+	<Emulator {roms} />
+
+	<section>
+		<h2>About</h2>
+		<p>
+			CHIP-8 is an interpreted programming language, developed by Joseph Weisbecker. It was
+			originally implemented for the COSMAC VIP in the mid-1970s, and was made to allow
+			videogames to be more easily programmed.
+		</p>
+	</section>
+
+	<section>
+		<h2>Controls</h2>
+		<p>
+			This emulator maps the keys in the original COSMAC VIP keypad to the modern QWERTY
+			keyboard.
+		</p>
+		<ControlsDiagram />
+	</section>
+
+	<section class="mb-2">
+		<h2>Links</h2>
+		<ul class="flex justify-center">
+			{#each links as { href, label }, i}
+				<li class="px-4 border-primary" class:border-l-2={i !== 0}>
+					<a {href}>{label}</a>
+				</li>
+			{/each}
+		</ul>
+	</section>
 </main>
